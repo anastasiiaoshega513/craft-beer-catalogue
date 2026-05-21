98a4c0c3f8e0aebb812caf6ec52a27774c963ac0
Steicy <anastasiaosega@gmail.com>
2026-05-21 13:36:47 +0300

    implemented search & filters

--- app/dependencies/enums.py
+++ app/dependencies/enums.py
@@ -4,3 +4,9 @@ from enum import StrEnum, auto
 class BeerTypeEnum(StrEnum):
     DARK = auto()
     LIGHT = auto()
+
+
+class AlcoholRangeEnum(StrEnum):
+    FOUR_SIX = "4-6"
+    SIX_EIGHT = "6-8"
+    EIGHT_PLUS = "8-plus"
