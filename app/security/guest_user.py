d1097cf69fb9e6996005fc04035d19c78f746017
Steicy <anastasiaosega@gmail.com>
2026-05-28 14:04:03 +0300

    implemented get/set unauthorized user guest_id cookie

---
+++ app/security/guest_user.py
@@ -0,0 +1,26 @@
+from uuid import uuid4
+from fastapi import Request, Response
+
+
+GUEST_COOKIE = "guest_id"
+COOKIE_MAX_AGE = 60 * 60 * 24 * 30
+
+
+def get_or_create_guest_id(request: Request, response: Response) -> str:
+    guest_id = request.cookies.get(GUEST_COOKIE)
+
+    if guest_id:
+        return guest_id
+
+    guest_id = str(uuid4())
+
+    response.set_cookie(
+        key=GUEST_COOKIE,
+        value=guest_id,
+        max_age=COOKIE_MAX_AGE,
+        httponly=True,
+        samesite="lax",
+        secure=False,
+    )
+
+    return guest_id
