68f477d138b329942ff2709f33db37cb5cc29bf0
Steicy <anastasiaosega@gmail.com>
2026-05-28 14:01:05 +0300

    implemented get token from http headers

--- app/security/jwt_token.py
+++ app/security/jwt_token.py
@@ -1,4 +1,5 @@
 from datetime import datetime, timedelta, timezone
+from fastapi import Request, HTTPException, status
 
 from jose import jwt, JWTError, ExpiredSignatureError
 
@@ -9,6 +10,27 @@ class TokenExpiredError(Exception):
 
 class InvalidTokenError(Exception):
     pass
+
+
+def get_token(request: Request) -> str:
+
+    authorization: str | None = request.headers.get("Authorization")
+
+    if not authorization:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Authorization header is missing"
+        )
+
+    scheme, _, token = authorization.partition(" ")
+
+    if scheme.lower() != "bearer" or not token:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Invalid Authorization header format. Expected 'Bearer <token>'"
+        )
+
+    return token
 
 
 class JWTAuthManager:
