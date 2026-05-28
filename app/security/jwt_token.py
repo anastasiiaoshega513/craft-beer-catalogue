f67bad83c0b8eb60e9b7c6910eb07dc101b84248
Steicy <anastasiaosega@gmail.com>
2026-05-28 13:29:10 +0300

    added create access & refresh tokens logic

---
+++ app/security/jwt_token.py
@@ -0,0 +1,48 @@
+from datetime import datetime, timedelta, timezone
+
+from jose import jwt, JWTError, ExpiredSignatureError
+
+
+class TokenExpiredError(Exception):
+    pass
+
+
+class InvalidTokenError(Exception):
+    pass
+
+
+class JWTAuthManager:
+
+    _ALGORITHM = "HS256"
+    _ACCESS_TOKEN_EXPIRE_MINUTES = 60
+    _REFRESH_TOKEN_EXPIRE_MINUTES = 60 * 24 * 7
+
+
+    def __init__(self, secret_key_access: str, secret_key_refresh: str) -> None:
+        self._secret_key_access = secret_key_access
+        self._secret_key_refresh = secret_key_refresh
+
+
+    def _create_token(self, data: dict, secret_key: str, expires_delta: timedelta) -> str:
+
+        to_encode = data.copy()
+        expire = datetime.now(timezone.utc) + expires_delta
+        to_encode.update({"exp": expire})
+        return jwt.encode(to_encode, secret_key, algorithm=self._ALGORITHM)
+
+
+    def create_access_token(self, data: dict) -> str:
+
+        return self._create_token(
+            data,
+            self._secret_key_access,
+            timedelta(minutes=self._ACCESS_TOKEN_EXPIRE_MINUTES)
+        )
+
+
+    def create_refresh_token(self, data: dict) -> str:
+
+        return self._create_token(
+            data,
+            self._secret_key_refresh,
+            timedelta(minutes=self._REFRESH_TOKEN_EXPIRE_MINUTES))
