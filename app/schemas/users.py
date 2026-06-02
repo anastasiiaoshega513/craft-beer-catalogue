ba7209eb34702b78aa200425d2a0086b619524dc
Steicy <anastasiaosega@gmail.com>
2026-06-02 20:15:31 +0300

    added base user schemas

---
+++ app/schemas/users.py
@@ -0,0 +1,34 @@
+from pydantic import BaseModel, EmailStr, field_validator
+
+from app.validators import users as validators
+
+
+class BaseEmailPasswordSchema(BaseModel):
+    email: EmailStr
+    password: str
+
+    @field_validator("password")
+    @classmethod
+    def validate_password(cls, value: str) -> str:
+        validators.validate_password_strength(value)
+        return value
+
+
+class UserRegistrationSchema(BaseEmailPasswordSchema):
+    pass
+
+
+class UserActivationSchema(BaseModel):
+    token: str
+
+
+class UserLoginSchema(BaseEmailPasswordSchema):
+    pass
+
+
+class UserUpdateSchema(BaseEmailPasswordSchema):
+    pass
+
+
+class MessageResponseSchema(BaseModel):
+    message: str
