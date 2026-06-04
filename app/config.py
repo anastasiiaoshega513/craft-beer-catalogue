aac46dada727f9591dc23edb24fb7e70fc83df01
Steicy <anastasiaosega@gmail.com>
2026-06-04 19:19:52 +0300

    added login endpoint

--- app/config.py
+++ app/config.py
@@ -1,6 +1,5 @@
 import os
 
-
 SMTP_HOST = os.getenv("SMTP_HOST", "localhost")
 SMTP_PORT = int(os.getenv("SMTP_PORT", "1025"))
 
@@ -9,3 +8,6 @@ SMTP_PASSWORD = os.getenv("SMTP_PASSWORD")
 SMTP_FROM_EMAIL = os.getenv("SMTP_FROM_EMAIL", "no-reply@example.com")
 
 FRONTEND_URL = os.getenv("FRONTEND_URL", "http://localhost:5173")
+
+JWT_ACCESS_SECRET_KEY = os.getenv("JWT_ACCESS_SECRET_KEY")
+JWT_REFRESH_SECRET_KEY = os.getenv("JWT_REFRESH_SECRET_KEY")
