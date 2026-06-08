227a81b766508dfe1aad96a4ca74eee2244f104f
Anastasiia <anastasiaosega@gmail.com>
2026-06-08 16:37:13 +0300

    added basic schemas

---
+++ app/schemas/carts.py
@@ -0,0 +1,26 @@
+from decimal import Decimal
+
+from pydantic import BaseModel, ConfigDict
+
+
+class MessageResponseSchema(BaseModel):
+    message: str
+
+
+class CartItemSchema(BaseModel):
+    model_config = ConfigDict(from_attributes=True)
+
+    id: int
+    name: str
+    quantity: int
+    price: Decimal
+    image_url: str
+
+
+class CartSchema(BaseModel):
+    model_config = ConfigDict(from_attributes=True)
+
+    id: int
+    cart_items: list[CartItemSchema]
+    subtotal: Decimal
+    total: Decimal
