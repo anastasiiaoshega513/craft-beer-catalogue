ca666aa280818b3db00ef4c408c2e83568a99319
Anastasiia <anastasiaosega@gmail.com>
2026-06-15 23:05:22 +0300

    added event types

--- app/dependencies/enums.py
+++ app/dependencies/enums.py
@@ -10,3 +10,11 @@ class AlcoholRangeEnum(StrEnum):
     FOUR_SIX = "4-6"
     SIX_EIGHT = "6-8"
     EIGHT_PLUS = "8-plus"
+
+
+class EventTypeEnum(StrEnum):
+    AFTER_WORK = "after-work"
+    WEEKEND_ESCAPE = "weekend-escape"
+    FRIENDS_OVER = "friends-over"
+    AFTER_MIDNIGHT = "after-midnight"
+    DINNER_AND_DRINKS = "dinner-and-drinks"
